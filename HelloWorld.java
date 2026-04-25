bf2802db96e89f33e67524235b7bca35ec08740f
rohitmahesh025-alt <rohitmahesh025@gmail.com>
2026-04-26 00:35:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Rohit Mahesh of Batch-43! and Batch-44 will start soon this year 2026");
+        System.out.println("Hello, Rohit Mahesh of Batch-43! and Batch-44 will start soon this year 2026 I have 60% completed AWS services now I am Aws cloud Engineer");
     }
 }
